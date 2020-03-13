53348d15d9cd7b7ab7060e9521e7ebfd80ceb023
Amantha <a.kalansooriya@lancaster.ac.uk>
2020-03-13 13:02:15 +0000

    made changes to rules

--- Board.java
+++ Board.java
@@ -8,11 +8,20 @@ public class Board implements ActionListener {
     private Square[][] squares = new Square[5][5];
     private int buttonsClicked = 0;
     private Square selectedSquare;
+    private Square middleSquare;
+
+    int x1;
+    int y1;
+    int x2;
+    int y2;
+    int i = 0;
+    int j = 0;
 
     public Board() {
         JFrame frame = new JFrame();
         frame.setTitle("Hoppers");
         frame.setSize(750, 750);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
         JPanel grid = new JPanel();
         frame.add(grid);
@@ -56,17 +65,40 @@ public class Board implements ActionListener {
         for (int i = 0; i < 5; i++)
             for (int j = 0; j < 5; j++)
                 if (buttonsClicked == 1)
-                    if (e.getSource().equals(squares[i][j].getButton())){
+                    if (e.getSource().equals(squares[i][j].getButton()) && "green frog".equals(squares[i][j].getName())){
                         System.out.println( squares[i][j].getX() + " " + squares[i][j].getY() );
                         selectedSquare = squares[i][j];
+                        x1 = squares[i][j].getX();
+                        y1 = squares[i][j].getY();
+                        System.out.println("BUTTON COUNT " + buttonsClicked);
+
+                        
+                        squares[i][j].setSquaretoFrogY();
+
+                    } else if(e.getSource().equals(squares[i][j].getButton()) && "red frog".equals(squares[i][j].getName())){
+                        x1 = squares[i][j].getX();
+                        y1 = squares[i][j].getY();
+                        
+                        selectedSquare = squares[i][j];
+                        squares[i][j].setSquaretoFrogRY();
 
                     }
-
+if (buttonsClicked == 2)
         for (int i = 0; i < 5; i++)
             for (int j = 0; j < 5; j++)
-                if (buttonsClicked == 2)
+                
                     if (e.getSource().equals(squares[i][j].getButton()))
-                        selectedSquare.moveFrog(squares[i][j]);
+                        x2 = squares[i][j].getX();
+                        y2 = squares[i][j].getY();
+                        middleSquare = squares[(x1+x2) / 2][(y1 + y2) / 2];
+                        System.out.println("X1 =  " + x1 + "Y1 =" + y1 + "X2 = " + x2 + "Y2 = " + y2);
+
+                        System.out.println("VALUE OF MIDDLE " + middleSquare.getName());
+                      
+                      
+                      // if()
+                       // only happen if frog is in the middle (greenf or redF)
+                       // selectedSquare.moveFrog(squares[i][j]);
 
         if (buttonsClicked == 2)
             buttonsClicked = 0;
